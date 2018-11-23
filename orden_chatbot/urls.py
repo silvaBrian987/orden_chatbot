02bafa70a31483eb1111e1c1763027539dc21228
Brian <silva.brian.987@gmail.com>
2018-11-22 23:55:47 -0300

    added JS facebook login and python facebook login

--- orden_chatbot/urls.py
+++ orden_chatbot/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 
 from . import views
 
-urlpatterns=[path('', views.index, name='index'),
-path('caballero/', views.caballero, name='caballero'),
+urlpatterns=[
+	path('', views.index, name='index'),
+	path('caballero/', views.caballero, name='caballero'),
+	path('loginFB/', views.loginFB, name='loginFB'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- orden_chatbot/views.py
+++ orden_chatbot/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template import loader
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 from .models import Caballero
+
+import facebook
+import json
 
 # Create your views here.
 
@@ -10,3 +14,16 @@ def index(request):
 	caballeros = Caballero.objects.all()
 	template = loader.get_template('orden_chatbot/index.html')
 	return HttpResponse(template.render({'caballeros': caballeros},request))
+def caballero(request):
+	caballeros = Caballero.objects.all()
+	arrCaballeros = []
+	for caballero in caballeros:
+		arrCaballeros.append({"nombre":caballero.nombre, "puto":caballero.puto})
+	return JsonResponse(arrCaballeros, safe=False)
+def loginFB(request):
+	data = json.loads(request.body.decode('utf-8'))
+	print(data)
+	graph = facebook.GraphAPI(access_token=data["authResponse"]["accessToken"], version="3.1")
+	permissions = graph.get_permissions(user_id=data["authResponse"]["userID"])
+	print(permissions)
+	return HttpResponse(str(permissions))
